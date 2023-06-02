0ff305af32b6d441554ae03f466aae835aee5d26
seunghyun333 <shb2in33@naver.com>
2023-06-02 17:45:18 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/Practice.java
+++ myapp/app/src/main/java/bitcamp/myapp/Practice.java
@@ -2,36 +2,37 @@ package bitcamp.myapp;
 
 import java.util.Scanner;
 
-public class Practive {
+public class Practice {
+  static Scanner scanner = new Scanner(System.in);
+
+  static final int MAX_SIZE = 100;
+  static int[] no = new int[MAX_SIZE];
+  static String[] name = new String[MAX_SIZE];
+  static String[] email = new String[MAX_SIZE];
+  static String[] password = new String[MAX_SIZE];
+  static char[] gender = new char[MAX_SIZE];
+
+  static int userId = 1;
+  static int length = 0;
+
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
 
-    final int MAX_SIZE = 100;
-    int userID = 1;
-    int length = 0;
-
-    int[] no = new int[MAX_SIZE];
-    String[] name = new String[MAX_SIZE];
-    String[] email = new String[MAX_SIZE];
-    String[] password = new String[MAX_SIZE];
-    char[] gender = new char[MAX_SIZE];
   }
 
   static void printTitle() {
     System.out.println("나의 목록 관리 시스템");
-    System.out.println("----------------------");
+    System.out.println("---------------------");
   }
 
-  static void inputMember(Scanner scanner, int i,
-      String[] name, String[] email, String[] password, char[] gender, int[] no, int userId) {
+  static void inputMember() {
     System.out.print("이름? ");
-    name[i] = scanner.next();
+    name[length] = scanner.next();
 
     System.out.print("이메일? ");
-    email[i] = scanner.next();
+    email[length] = scanner.next();
 
     System.out.print("암호? ");
-    password[i] = scanner.next();
+    password[length] = scanner.next();
 
     loop: while (true) {
       System.out.println("성별: ");
@@ -43,19 +44,21 @@ public class Practive {
 
       switch (menuNo) {
         case "1":
-          gender[i] = 'M';
+          gender[length] = 'M';
           break loop;
         case "2":
-          gender[i] = 'W';
+          gender[length] = 'W';
           break loop;
         default:
-          System.out.println("무효한 번호입니다.");
+          System.out.println("무효한 번호 입니다.");
       }
     }
-    no[i] = userId++;
+
+    no[length] = userId++;
+    length++;
   }
 
-  static boolean promptContinue(Scanner scanner) {
+  static boolean promptContinue() {
     System.out.print("계속 하시겠습니까?(Y/n) ");
     String response = scanner.nextLine();
     if (!response.equals("") && !response.equalsIgnoreCase("Y")) {
@@ -64,13 +67,14 @@ public class Practive {
     return true;
   }
 
-  static void printMembers(int length, int[] no, String[] name, String[] email, char[] gender) {
-    System.out.println("=====================");
+  static void printMembers() {
+    System.out.println("===================");
     System.out.printf("번호, 이름, 이메일, 성별 \n");
-    System.out.println("======================");
+    System.out.println("===================");
 
     for (int i = 0; i < length; i++) {
       System.out.printf("%d, %s, %s, %c\n", no[i], name[i], email[i], gender[i]);
     }
   }
+
 }
