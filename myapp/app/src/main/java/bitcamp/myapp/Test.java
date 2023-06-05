435e996397abcf01fe6a793fe0901d790d442048
seunghyun333 <shb2in33@naver.com>
2023-06-05 10:47:52 +0900

    ,

--- myapp/app/src/main/java/bitcamp/myapp/Test.java
+++ myapp/app/src/main/java/bitcamp/myapp/Test.java
@@ -5,7 +5,27 @@ public class Test {
     // 2 * 3 + 7 - 2 / 2 = ?
     // => 연산자 우선 순위를 고려하지 않고 앞에서부터 뒤로 순차적으로 계산한다.
 
-    int result = (2 * 3 + 7 - 2) / 2;
+    int result = multiple(2, 3);
+    result = plus(result, 7);
+    result = minus(result, 2);
+    result = divide(result, 2);
     System.out.println(result);
   }
+
+  static int plus(int a, int b) {
+    return a + b;
+  }
+
+  static int minus(int a, int b) {
+    return a - b;
+  }
+
+  static int multiple(int a, int b) {
+    return a * b;
+  }
+
+  static int divide(int a, int b) {
+    return a / b;
+  }
+
 }
