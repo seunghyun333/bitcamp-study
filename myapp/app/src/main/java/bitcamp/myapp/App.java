2660192c5164c72a8d6be5ac7b1b57750ff65070
seunghyun333 <shb2in33@naver.com>
2023-06-05 14:26:35 +0900

    ,

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -1,36 +1,21 @@
 package bitcamp.myapp;
 
-import java.util.Scanner;
-
 public class App {
-  static Scanner scanner = new Scanner(System.in);
-
-  static final int MAX_SIZE = 100;
-  static int[] no = new int[MAX_SIZE];
-  static String[] name = new String[MAX_SIZE];
-  static String[] email = new String[MAX_SIZE];
-  static String[] password = new String[MAX_SIZE];
-  static char[] gender = new char[MAX_SIZE];
-  static int userId = 1;
-  static int length = 0;
-
-  static final char MALE = 'M';
-  static final char FEMALE = 'W';
 
   public static void main(String[] args) {
 
     printTitle();
 
-    while (length < MAX_SIZE) {
-      inputMember();
+    while (MemberHandler.length < MemberHandler.MAX_SIZE) {
+      MemberHandler.inputMember();
       if (!promptContinue()) {
         break;
       }
     }
 
-    printMembers();
+    MemberHandler.printMembers();
 
-    scanner.close();
+    Prompt.scanner.close();
 
   }
 
@@ -39,55 +24,12 @@ public class App {
     System.out.println("-----------------------------");
   }
 
-  static void inputMember() {
-    name[length] = prompt("이름? ");
-    email[length] = prompt("이메일? ");
-    password[length] = prompt("암호? ");
-
-    loop: while (true) {
-      String menuNo = prompt("성별:\n " +
-          " 1. 남자\n " +
-          " 2. 여자\n " +
-          " >");
-
-      switch (menuNo) {
-        case "1":
-          gender[length] = MALE;
-          break loop;
-        case "2":
-          gender[length] = FEMALE;
-          break loop;
-        default:
-          System.out.println("무효한 번호 입니다");
-
-      }
-    }
-
-    no[length] = userId++;
-    length++;
-  }
-
   static boolean promptContinue() {
-    String response = prompt("계속 하시겠습니까?(Y/n) ");
+    String response = Prompt.inputString("계속 하시겠습니까?(Y/n) ");
     if (!response.equals("") && !response.equalsIgnoreCase("Y")) {
       return false;
     }
     return true;
-  }
-
-  static void printMembers() {
-    System.out.println("============================");
-    System.out.printf("번호, 이름, 이메일 , 성별 \n");
-    System.out.println("============================");
-
-    for (int i = 0; i < length; i++) {
-      System.out.printf("%d, %s, %s, %c\n", no[i], name[i], email[i], gender[i]);
-    }
-  }
-
-  static String prompt(String title) {
-    System.out.print(title);
-    return scanner.nextLine();
   }
 
 }
