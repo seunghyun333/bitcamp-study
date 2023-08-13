02ed35387a2250c4a3ac78e6198c7178cb7abd97
seunghyun333 <shb2in33@naver.com>
2023-08-13 21:29:44 +0900

    .

---
+++ personalapp1/app-server/doc/model/data.sql
@@ -0,0 +1,22 @@
+-- diary 테이블 예제 데이터
+
+-- 일기
+dia_diary ;
+
+-- 회원
+dia_member ;
+
+-- 응원게시판
+dia_board ;
+
+-- 댓글
+dia_comment ;
+
+-- 대표사진
+dia_pic ;
+
+-- 공지사항
+dia_notice ;
+
+-- 콘텐트
+dia_content ;
