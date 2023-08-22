7aaa61db197ea6a084df3f258865c6b21c8bfbeb
seunghyun333 <shb2in33@naver.com>
2023-08-22 20:26:08 +0900

    첨부파일을 네이버 클라우드 스토리지 서비스에 저장하기

--- personalapp1/app-60-server/src/main/java/bitcamp/personalapp/handler/BoardUpdateServlet.java
+++ personalapp1/app-60-server/src/main/java/bitcamp/personalapp/handler/BoardUpdateServlet.java
@@ -3,6 +3,7 @@ package bitcamp.personalapp.handler;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.UUID;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
@@ -14,10 +15,9 @@ import bitcamp.personalapp.vo.AttachedFile;
 import bitcamp.personalapp.vo.Board;
 import bitcamp.personalapp.vo.Member;
 
-
-@WebServlet("/board/add")
+@WebServlet("/board/update")
 @MultipartConfig(maxFileSize = 1024 * 1024 * 10)
-public class BoardAddServlet extends HttpServlet {
+public class BoardUpdateServlet extends HttpServlet {
 
   private static final long serialVersionUID = 1L;
 
@@ -31,62 +31,66 @@ public class BoardAddServlet extends HttpServlet {
       return;
     }
 
+    response.setContentType("text/html;charset=UTF-8");
+    PrintWriter out = response.getWriter();
+    out.println("<!DOCTYPE html>");
+    out.println("<html>");
+    out.println("<head>");
+    out.println("<meta charset='UTF-8'>");
+    out.println("<title>게시글</title>");
+    out.println("</head>");
+    out.println("<body>");
+    out.println("<h1>게시글 변경</h1>");
+
+
+
     try {
       Board board = new Board();
-      board.setMno(loginUser);
+      board.setNo(Integer.parseInt(request.getParameter("no")));
       board.setTitle(request.getParameter("title"));
       board.setContent(request.getParameter("content"));
-      board.setSecret(Boolean.parseBoolean(request.getParameter("secret")));
+      String secretParam = request.getParameter("secret");
+      boolean isSecret = Boolean.parseBoolean(secretParam);
+      board.setSecret(isSecret);
+      board.setMno(loginUser);
 
 
-
+      String uploadDir = request.getServletContext().getRealPath("/upload/board/");
       ArrayList<AttachedFile> attachedFiles = new ArrayList<>();
 
       for (Part part : request.getParts()) {
-        // System.out.println(part.getName());
         if (part.getName().equals("files") && part.getSize() > 0) {
-          String uploadFileUrl = InitServlet.ncpObjectStorageService
-              .uploadFile("bitcamp-nc7-bucket-07", "board/", part);
+          String filename = UUID.randomUUID().toString();
+          part.write(uploadDir + filename);
           AttachedFile attachedFile = new AttachedFile();
-          attachedFile.setFilePath(uploadFileUrl);
+          attachedFile.setFilePath(filename);
           attachedFiles.add(attachedFile);
         }
       }
       board.setAttachedFiles(attachedFiles);
 
-      response.setContentType("text/html;charset=UTF-8");
-      PrintWriter out = response.getWriter();
-      out.println("<!DOCTYPE html>");
-      out.println("<html>");
-      out.println("<head>");
-      out.println("<meta charset='UTF-8'>");
-      out.println("<meta http-equiv='refresh' content='1;url=/board/list'>");
-      out.println("<title>게시글</title>");
-      out.println("</head>");
-      out.println("<body>");
-      out.println("<h1>게시글 등록</h1>");
 
-      try {
-        InitServlet.boardDao.insert(board);
-
+      if (InitServlet.boardDao.update(board) == 0) {
+        out.println("<p>게시글이 없거나 변경 권한이 없습니다.</p>");
+      } else {
         if (attachedFiles.size() > 0) {
+          // 게시글을 정상적으로 변경했으면, 그 게시글의 첨부파일을 추가한다.
           int count = InitServlet.boardDao.insertFiles(board);
           System.out.println(count);
         }
-        InitServlet.sqlSessionFactory.openSession(false).commit();
-        out.println("<p>등록 성공입니다!</p>");
 
-      } catch (Exception e) {
-        InitServlet.sqlSessionFactory.openSession(false).rollback();
-        out.println("<p>등록 실패입니다!</p>");
-        e.printStackTrace();
+        out.println("<p>변경했습니다.</p>");
+        response.setHeader("refresh", "1;url=/board/list");
       }
-      out.println("</body>");
-      out.println("</html>");
+      InitServlet.sqlSessionFactory.openSession(false).commit();
 
     } catch (Exception e) {
-      throw new ServletException(e);
+      InitServlet.sqlSessionFactory.openSession(false).rollback();
+      out.println("<p>게시글 변경 실패입니다.</p>");
+      e.printStackTrace();
     }
+    out.println("</body>");
+    out.println("</html>");
   }
 }
 
