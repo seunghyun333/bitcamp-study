ffdb9fa9d48c4fec214770e43eb4bd29fc276ce3
seunghyun333 <shb2in33@naver.com>
2023-08-24 23:07:14 +0900

    LoginFormServlet checkbox 추가

--- personalapp1/app-server/src/main/java/bitcamp/personalapp/handler/LoginServlet.java
+++ personalapp1/app-server/src/main/java/bitcamp/personalapp/handler/LoginServlet.java
@@ -3,6 +3,7 @@ package bitcamp.personalapp.handler;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,6 +21,16 @@ public class LoginServlet extends HttpServlet {
     Member m = new Member();
     m.setEmail(request.getParameter("email"));
     m.setPw(request.getParameter("pw"));
+    
+    if (request.getParameter("saveEmail") != null) {
+    	Cookie cookie = new Cookie("email", m.getEmail());
+    	response.addCookie(cookie);
+    } else {
+    	Cookie cookie = new Cookie("email", "no");
+    	cookie.setMaxAge(0);
+    	response.addCookie(cookie);
+    }
+
 
 
     Member loginUser = InitServlet.memberDao.findByIdAndPassword(m);
@@ -31,7 +42,7 @@ public class LoginServlet extends HttpServlet {
 
 
     request.setAttribute("message", "회원 정보가 일치하지 않습니다.");
-    request.setAttribute("refresh", "1;url=/auth/form.html");
+    request.setAttribute("refresh", "1;url=/auth/form");
 
     request.getRequestDispatcher("/error").forward(request, response);
   }
