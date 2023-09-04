7800fca2fca72f561f541ea877cbdbd9898c8864
seunghyun333 <shb2in33@naver.com>
2023-09-04 21:27:39 +0900

    72. Spring의 TransactionTemplate 으로 교체하기

---
+++ personalapp1/app-72-server/src/main/java/bitcamp/personalapp/controller/MemberUpdateController.java
@@ -0,0 +1,51 @@
+package bitcamp.personalapp.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import bitcamp.personalapp.service.MemberService;
+import bitcamp.personalapp.service.NcpObjectStorageService;
+import bitcamp.personalapp.vo.Member;
+
+@Controller("/member/update")
+public class MemberUpdateController implements PageController {
+
+  @Autowired
+  MemberService memberService;
+
+  @Autowired
+  NcpObjectStorageService ncpObjectStorageService;
+
+  @Override
+  public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    try {
+      Member member = new Member();
+      member.setNo(Integer.parseInt(request.getParameter("no")));
+      member.setName(request.getParameter("name"));
+      member.setEmail(request.getParameter("email"));
+      member.setPw(request.getParameter("pw"));
+
+
+      Part photoPart = request.getPart("photo");
+      if (photoPart.getSize() > 0) {
+        String uploadFileUrl =
+            ncpObjectStorageService.uploadFile("bitcamp-nc7-bucket-07", "member/", photoPart);
+        member.setPhoto(uploadFileUrl);
+      }
+
+
+      if (memberService.update(member) == 0) {
+        throw new Exception("<p>회원이 없습니다.</p>");
+      } else {
+        return "redirect:list";
+
+      }
+
+    } catch (Exception e) {
+      request.setAttribute("refresh", "2;url=list");
+      throw e;
+    }
+  }
+}
--- personalapp1/app-server/src/main/java/bitcamp/util/TransactionCallback.java
+++
@@ -1,8 +0,0 @@
-package bitcamp.util;
-
-import org.springframework.transaction.TransactionStatus;
-
-public interface TransactionCallback<T> {
-  T doInTransaction(TransactionStatus stuats);
-
-}
