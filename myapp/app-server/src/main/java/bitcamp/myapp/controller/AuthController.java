aa4950966cc6511e0dfdb942d75e377e4cedc21e
seunghyun333 <shb2in33@naver.com>
2023-09-13 18:07:46 +0900

    thymeleaf

--- myapp/app-server/src/main/java/bitcamp/myapp/controller/AuthController.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/controller/AuthController.java
@@ -5,6 +5,7 @@ import bitcamp.myapp.vo.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,7 +26,8 @@ public class AuthController {
 
 
   @GetMapping("form")
-  public void form() throws Exception {
+  public void form(@CookieValue(defaultValue = "") String email, Model model) {
+    model.addAttribute("email", email);
   }
 
   @PostMapping("login")
